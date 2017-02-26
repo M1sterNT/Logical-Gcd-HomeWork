08b9a4c0c93e03f5f0af23317f705cde8400f045
WANDEE SREPENRAK <59050300@kmitl.ac.th>
2017-02-26 15:48:53 +0700

    Added : All Source
    Add Project

---
+++ Logical Gcd HomeWork/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Gcd_HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input NumberA : ");
+            int numberA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input NumberB : ");
+            int numberB = Convert.ToInt32(Console.ReadLine());
+            if (numberB > numberA)
+            {
+                int tem = numberA;
+                numberA = numberB;
+                numberB = tem;
+            }
+            Console.WriteLine("GCD : {0} ", Gcd(numberA, numberB));
+            Console.ReadLine();
+
+        }
+        static int Gcd(int numberA , int numberB) {
+            if (numberB == 0)
+                return numberA;
+            return Gcd(numberB, numberA % numberB);
+        }
+    }
+}
